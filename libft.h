4fc414c95a31222236a0952e60f50c7041ed77bc
Axel Coézard <axel.coezard@gmail.com>
2021-10-06 11:45:23 +0200

    ft_putnbr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acoezard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 12:17:38 by acoezard          #+#    #+#             */
-/*   Updated: 2021/10/05 12:40:08 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/10/06 11:26:23 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 void 	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
