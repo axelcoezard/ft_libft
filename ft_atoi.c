e558fd438184db70a8b7d5d8d697fc0c5ff97883
Axel Coézard <axel.coezard@gmail.com>
2021-10-06 12:32:10 +0200

    ajout de ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acoezard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 12:28:27 by acoezard          #+#    #+#             */
+/*   Updated: 2021/10/06 12:30:54 by acoezard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	nbr;
+
+	while (*nptr == ' ' || *nptr == '\t' || *nptr == '\v'
+		|| *nptr == '\n' || *nptr == '\f' || *nptr == '\r')
+		nptr++;
+	sign = 1;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	nbr = 0;
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		nbr *= 10;
+		nbr += *nptr - '0';
+		nptr++;
+	}
+	return (sign * nbr);
+}
