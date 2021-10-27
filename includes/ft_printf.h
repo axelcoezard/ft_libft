b9896f6324984715298792f9c0a2e325d5868588
Axel Coezard <acoezard@c2r4p5.42nice.fr>
2021-10-27 15:02:05 +0200

    tried_a_thing

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: acoezard <acoezard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 11:35:31 by acoezard          #+#    #+#             */
-/*   Updated: 2021/10/27 11:38:58 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/10/27 15:01:46 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ struct s_flags
 };
 typedef struct s_flags	t_flags;
 
-int		ft_printf(const char *format, ...);
-
 int		ft_printf_char(va_list params, t_flags flags);
 int		ft_printf_string(va_list params, t_flags flags);
 int		ft_printf_ptr(va_list params, t_flags flags);
@@ -31,5 +29,7 @@ int		ft_printf_integer(va_list params, t_flags flags);
 int		ft_printf_udecimal(va_list params, t_flags flags);
 int		ft_printf_hex(va_list params, t_flags flags);
 int		ft_printf_percent(va_list params, t_flags flags);
+
+int		ft_printf(const char *format, ...);
 
 #endif
