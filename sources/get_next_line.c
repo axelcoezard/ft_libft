07215a8cda79fa45129cbfd79e268df32bea9217
Axel Coezard <acoezard@c2r6p5.42nice.fr>
2021-12-03 16:23:03 +0100

    issue: free line at end

--- sources/get_next_line.c
+++ sources/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acoezard <acoezard@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 16:10:20 by acoezard          #+#    #+#             */
-/*   Updated: 2021/12/02 19:39:04 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/12/03 16:22:33 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,12 @@ char	*get_next_line(int fd)
 	{
 		reader = read(fd, byte, 1);
 		byte[1] = 0;
-		ft_putchar_fd(byte[0], 1);
 		line = ft_strjoin(line, byte);
+	}
+	if (line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
 	}
 	return (line);
 }
@@ -34,7 +38,8 @@ int	main(void)
 	char *line;
 	int fd;
 
-	fd = open("ft_strjoin.c", O_RDONLY);
-	line = get_next_line(fd);
-	line = get_next_line(fd);
+	fd = open("../Makefile", O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL)
+		ft_putstr_fd(line, 1);
+	close(fd);
 }
